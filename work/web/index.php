41882e8e8b3e81608bec71e76797c3df2c297f59
nikawadori <nikawa.2161.t@gmail.com>
2021-03-12 17:24:13 +0900

    trimと条件演算子を使って空文字の際に表示を切り替える

--- work/web/index.php
+++ work/web/index.php
@@ -8,7 +8,6 @@ include ('../app/_parts/_header.php');
 
 <form action="result.php" method="get">
   <input type="text" name="message">
-  <input type="text" name="username">
   <button>ボタン</button>
 </form>
 
