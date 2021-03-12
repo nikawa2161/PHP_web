c4092d67076b0a5aad2ca4730901dea830f6da42
nikawadori <nikawa.2161.t@gmail.com>
2021-03-12 17:12:40 +0900

    formの作成

--- work/web/index.php
+++ work/web/index.php
@@ -6,5 +6,11 @@ include ('../app/_parts/_header.php');
 
 ?>
 
+<form action="result.php" method="get">
+  <input type="text" name="message">
+  <input type="text" name="username">
+  <button>ボタン</button>
+</form>
+
 <?php
  include ('../app/_parts/_footer.php');
---
+++ work/web/result.php
@@ -0,0 +1,17 @@
+<?php
+
+require ('../app/functions.php');
+
+$message = filter_input(INPUT_GET, 'message');
+$username = filter_input(INPUT_GET, 'username');
+
+include ('../app/_parts/_header.php');
+
+?>
+
+<p><?= h($message); ?> by <?= h($username); ?></p>
+<p><a href="index.php">戻る</a></p>
+
+
+<?php
+ include ('../app/_parts/_footer.php');
