68407ef92b2e901e2d7df892829e92810562cf4b
nikawadori <nikawa.2161.t@gmail.com>
2021-03-12 17:00:40 +0900

    使い回すコードを別ファイルで管理

--- work/web/index.php
+++ work/web/index.php
@@ -2,28 +2,9 @@
 
 require ('../app/functions.php');
 
-$names = [
-  'Taro', 'Jiro', 'Saburo'
-  ];
+include ('../app/_parts/_header.php');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>PHP Practice</title>
-</head>
-<body>
-  <ul>
-    <?php if (empty($names)): ?>
-      <li>配列に要素が入っていません</li>
-    <?php else: ?>
-      <?php foreach ($names as $name): ?>
-        <li><?= h($name); ?></li>
-      <?php endforeach; ?>
-    <?php endif; ?>
-
-  </ul>
-</body>
-</html>
+<?php
+ include ('../app/_parts/_footer.php');
