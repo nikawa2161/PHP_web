3a3f3822b497ac07a7f03f820850d45a1703d08f
nikawadori <nikawa.2161.t@gmail.com>
2021-03-12 16:14:03 +0900

    initialコミット

---
+++ work/web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <p>Hello, PHP!</p>
+  <!-- 今日の日時と曜日を表示 -->
+  <p>Today: <?php echo date('Y-m-d H:i:s l')?></p>
+</body>
+</html>
